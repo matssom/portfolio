a7b29d183428b3e5af8339f11a7c359e4134c20b
Mats Sommervold <mats.sommervold@gmail.com>
2021-01-16 12:28:53 +0100

    Added image to work cards

--- src/routes/Work.svelte
+++ src/routes/Work.svelte
@@ -24,8 +24,13 @@ import Li from '../components/Type/Li.svelte';
             <Card>
                 <Container padding size="large">
                     <Stack size="medium">
-                        <H3>Teacher Assistant | Oslo Metropolitan University</H3>
-                        <Sub small>2020-Present</Sub>
+                        <div class="header">
+                            <div>
+                                <H3>Teacher Assistant | Oslo Metropolitan University</H3>
+                                <Sub small>2020-Present</Sub>
+                            </div>
+                            <img class="job-image" src="https://res-3.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/bjd0kn1ddocmdmmcculj" alt="oslomet logo">
+                        </div>
                         <P>I am currently a part-time employee at Oslo Metropolitan University. This semester I am a teacher assistant in the Web programming class. My tasks fluctuate between teaching new topics to a group of 300+ students, reviewing homework, consulting students one on one and grading obligatory assignments. The class I am currently assistant in focuses on the interaction between javascript on the frontend and java on the backend.
                         </P>
                         <P>Last semesester I did the same jobe in a class that is the prerequisite to this course, "Universal design and development". This course foucused more on the basics of HTML and CSS</P>
@@ -36,8 +41,13 @@ import Li from '../components/Type/Li.svelte';
             <Card>
                 <Container padding size="large">
                     <Stack size="medium">
-                        <H3>Frontend Developer | Coness AS</H3>
-                        <Sub small>2017-2019</Sub>
+                        <div class="header">
+                            <div>
+                                <H3>Frontend Developer | Coness AS</H3>
+                                <Sub small>2017-2019</Sub>
+                            </div>
+                            <img class="job-image" src="https://zenprospect-production.s3.amazonaws.com/uploads/pictures/5ed5b54be9641d000118785e/picture" alt="coness logo">
+                        </div>
                         <P>Being a frontend developer for the consulting firm Coness AS was my first programming job. The work consisted of designing, developing and maintaining the website for Coness (<Clickable to="https://coness.no" text="Coness.no"/>). I worked with mostly HTML, CSS & javascript. The experience I got from this job continues to be valuable for me to this day.
                         </P>
                     </Stack>
@@ -46,8 +56,13 @@ import Li from '../components/Type/Li.svelte';
             <Card>
                 <Container padding size="large">
                     <Stack size="medium">
-                        <H3>Judo Coach | Ippon Judo Club</H3>
-                        <Sub small>2016-Present</Sub>
+                        <div class="header">
+                            <div>
+                                <H3>Judo Coach | Ippon Judo Club</H3>
+                                <Sub small>2016-Present</Sub>
+                            </div>
+                            <img class="job-image" src="https://www.ippon.no/uploads/2/5/1/7/25172525/ippon-logo-100x130.png" alt="ippon jk logo">
+                        </div>
                         <P>Ever since I was a kid I have practiced Judo. That has transated into a Coaching job that I have had for the last 4 years. My tasks as a judo coach are:</P>
                         <Ul>
                             <Li>Coaching kids (age 6 - 14) in the art of Judo and all its values. (respect, friendship, courage, self-control, sincerity, and courtesy)</Li>
@@ -63,5 +78,14 @@ import Li from '../components/Type/Li.svelte';
 </Section>
 
 <style>
-    
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .job-image {
+        /* height: 7rem; */
+        width: 7rem;
+    }
 </style>
