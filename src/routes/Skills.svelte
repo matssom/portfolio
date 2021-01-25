c0808c352b3f065165f45107a0752a2fc313fc95
Mats Sommervold <mats.sommervold@gmail.com>
2021-01-25 17:06:16 +0100

    Added some soft skills

--- src/routes/Skills.svelte
+++ src/routes/Skills.svelte
@@ -1,5 +1,6 @@
 <script>
     import Container from "../components/Layout/Container.svelte";
+    import Clickable from '../components/Feature/Clickable.svelte';
     import Section from "../components/Layout/Section.svelte";
     import Split from "../components/Layout/Split.svelte";
     import Stack from '../components/Layout/Stack.svelte';
@@ -11,61 +12,55 @@
 </script>
 
 <Section>
+    <Container padding horizontal narrow>
+        <Stack size="large">
+            <H1 icon="open-book">Soft Skills</H1>
+            <Split col="2">
+                <P>While knowing hard programming skills is valuable, you won't be able to create solutions to today's complex problems without collaboration and project management.</P>
+                <div class="image-holder">
+                    <img class="coding-image" src="assets/routes/skills/scrum.svg" alt="">
+                </div>
+            </Split>
+        </Stack>
+    </Container>
+</Section>
+<Section>
     <Container padding horizontal>
         <Stack size="large">
-            <H1 center>Skills</H1>
-            <Split col="3">
+            <Split col="2">
                 <Container padding horizontal>
                     <Stack>
-                        <H3>Algorithms & Datastructures</H3>
-                        <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, mollitia expedita ipsum tempore doloribus tenetur impedit magnam sunt accusantium perspiciatis suscipit provident dignissimos repellendus cumque?</P>
-                    </Stack>
-                </Container>
-                <Container padding horizontal>
-                    <Stack>
-                        <H3>API's</H3>
-                        <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, mollitia expedita ipsum tempore doloribus tenetur impedit magnam sunt accusantium perspiciatis suscipit provident dignissimos repellendus cumque?</P>
-                    </Stack>
-                </Container>
-                <Container padding horizontal>
-                    <Stack>
-                        <H3>Headless CMS's</H3>
-                        <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, mollitia expedita ipsum tempore doloribus tenetur impedit magnam sunt accusantium perspiciatis suscipit provident dignissimos repellendus cumque?</P>
-                    </Stack>
-                </Container>
-                <Container padding horizontal>
-                    <Stack>
-                        <H3>Npm development</H3>
-                        <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, mollitia expedita ipsum tempore doloribus tenetur impedit magnam sunt accusantium perspiciatis suscipit provident dignissimos repellendus cumque?</P>
-                    </Stack>
-                </Container>
-                <Container padding horizontal>
-                    <Stack>
-                        <H3>User experience design (UX)</H3>
-                        <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, mollitia expedita ipsum tempore doloribus tenetur impedit magnam sunt accusantium perspiciatis suscipit provident dignissimos repellendus cumque?</P>
+                        <H3>Agile Development</H3>
+                        <P>According to the <Clickable to="https://stateofagile.com/" text="state of agile 2020"/> report, agile is here to stay. It has become vital to be adaptable and flexable in todays society where requirements change faster than ever.I have agile background from the System Development course at OsloMet.</P>
                     </Stack>
                 </Container>
                 <Container padding horizontal>
                     <Stack>
                         <H3>Testing</H3>
+                        <P>What does it mean to be professional? In some sense, a professional is just someone who takes responsibility for their choices. Testing is, by all means, a hard skill. But it is also a way to be able to trust that you are delivering quality code.</P>
+                    </Stack>
+                </Container>
+                <Container padding horizontal>
+                    <Stack>
+                        <H3>Lifelong Learner</H3>
+                        <P>The joy of learning is the single most important reason for why I started programming. I have allways wanted to learn new ways to create human-centric experiences.</P>
+                    </Stack>
+                </Container>
+                <Container padding horizontal>
+                    <Stack>
+                        <H3>Bussiness mindedness</H3>
                         <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, mollitia expedita ipsum tempore doloribus tenetur impedit magnam sunt accusantium perspiciatis suscipit provident dignissimos repellendus cumque?</P>
                     </Stack>
                 </Container>
                 <Container padding horizontal>
                     <Stack>
-                        <H3>Cryptography</H3>
+                        <H3>Curiosity</H3>
                         <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, mollitia expedita ipsum tempore doloribus tenetur impedit magnam sunt accusantium perspiciatis suscipit provident dignissimos repellendus cumque?</P>
                     </Stack>
                 </Container>
                 <Container padding horizontal>
                     <Stack>
-                        <H3>CI/CD</H3>
-                        <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, mollitia expedita ipsum tempore doloribus tenetur impedit magnam sunt accusantium perspiciatis suscipit provident dignissimos repellendus cumque?</P>
-                    </Stack>
-                </Container>
-                <Container padding horizontal>
-                    <Stack>
-                        <H3>Full stack development</H3>
+                        <H3>Interpersonal skills</H3>
                         <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, mollitia expedita ipsum tempore doloribus tenetur impedit magnam sunt accusantium perspiciatis suscipit provident dignissimos repellendus cumque?</P>
                     </Stack>
                 </Container>
@@ -75,5 +70,22 @@
 </Section>
 
 <style>
-    
+    .image-holder {
+        position: relative;
+    }
+
+    .coding-image {
+        display: none;
+    }
+
+    @media (min-width: 53.75em) {
+        .coding-image {
+            display: block;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-10%, -75%);
+            width: 55%;
+        }
+    }
 </style>
